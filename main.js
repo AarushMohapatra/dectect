ab1be14d8b226a7c549582e781af09a1f679436b
AarushMohapatra <103845656+AarushMohapatra@users.noreply.github.com>
2023-01-25 19:06:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+img = "";
+objects = [];
+status = "";
+function preload( ) {
+img = loadImage('R.jpeg');
+}
+function setup(){
+  canvas = createCanvas(640, 420);
+  canvas.center();0
+  video = createCapture(VIDEO);
+  video.size(640,420);
+  video.hide();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "i know your IP address.....";
+}
+function draw(){
+  image(img, 0, 0, 640, 420);
+}
+function modelLoaded(){
+  console. log("Model Loaded!")
+status = true;
+objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results){
+  if(error){
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+function draw(){
+  image(video, 0, 0, 640, 420);
+  if(status != ""){
+    r=random(255);
+    g=random(255);
+    b=random(255);
+    objectDetector.detect(video, gotResult);
+    for(i=0;i<objects.length; i++){
+      document.getElementById("status").innerHTML = "found you >:)";
+      document.getElementById("number_of_objects").innerHTML = "Number of objects dectected are: " + objects.length;
+
+      fill(r,g,b);
+      percent = floor(objects[i].confidence*100);
+      text(objects[i].label + " " + percent + "%",objects[i].x + 15, objects[i].y + 15);
+      noFill();
+      stroke(r,g,b);
+      rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+    }
+  }
+}
